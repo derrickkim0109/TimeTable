73d723b810c5ec136da605d19a54cd8f4606b060
derrickkim0109 <derrickkim0109@naver.com>
2023-05-07 09:50:35 +0900

    feat: Infrastructure 레이어 생성 및 네트워크 처리 기능 생성

---
+++ CalendarApp/Infrastructure/Network/Infrastructure/Network/Endpoint/BodyEncoding.swift
@@ -0,0 +1,11 @@
+//
+//  BodyEncoding.swift
+//  CalendarApp
+//
+//  Created by Derrick kim on 2023/05/07.
+//
+
+enum BodyEncoding {
+    case jsonSerializationData
+    case stringEncodingAscii
+}
