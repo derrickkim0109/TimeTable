5d889a2f8d6d1494eccccb283a7f527dcf9ff9bd
derrickkim0109 <derrickkim0109@naver.com>
2023-05-07 09:35:10 +0900

    add: 초기 프로젝트 설정

---
+++ CalendarApp/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CalendarApp
+//
+//  Created by Derrick kim on 2023/05/07.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
